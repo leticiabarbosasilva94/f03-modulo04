3b1b8f604f416b655e7dc2924cb1e0299b41d349
Letícia Barbosa Silva <leticiabarbosasilva94@gmail.com>
2019-11-25 21:52:56 -0300

    Finished configuring webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,20 @@
-module.exports = {
+const { resolve } = require('path');
 
+module.exports = {
+  entry: resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
 };
